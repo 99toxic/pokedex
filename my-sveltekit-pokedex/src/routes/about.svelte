c6d23ddae6ca29a399765d8a1f093ef2556a912e
Nick Simpson <nsimpson1983@gmail.com>
2021-11-13 23:17:16 -0500

    Delete my-sveltekit-pokedex directory

--- my-sveltekit-pokedex/src/routes/about.svelte
+++
@@ -1,5 +0,0 @@
-<svelte:head>
-  <title>About | Selte Kit Pokedex</title>
-</svelte:head>
-<h1 class="text-4xl text-center my-8 uppercase">About</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem iusto labore ipsam corporis maxime laudantium, doloribus possimus repellendus consequatur neque nisi architecto harum in? Recusandae unde nam iure ipsa voluptatem!</p>
